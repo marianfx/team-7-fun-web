3af3fd10a483670445987fb0e1aac1925989c9d3
FX <marian.focsa@outlook.com>
2016-05-19 15:27:37 +0300

    Added logger table, logging triggers and updated data manipulation
    Ieiii.

--- DB_Oracle/PCK_Data_Manipulation.sql
+++ DB_Oracle/PCK_Data_Manipulation.sql
@@ -1,4 +1,3 @@
-
 -- ########## The Data Manipulation Package - HEAD #########
 -- Obs - a folder  C:\odata is required --
 DROP PACKAGE data_manipulation;/
@@ -17,14 +16,14 @@ CREATE OR REPLACE PACKAGE data_manipulation IS
 		PROCEDURE WRITE_TO_FILE(DICTIONARY IN KVDICT, FILE_NAME VARCHAR2);
 		PROCEDURE WRITE_DATA_TO_CSV(TAB_NAME IN VARCHAR2);
 		PROCEDURE EXPORT_ALL_TABLES_TO_CSV;
-		PROCEDURE  populate_player;
-		PROCEDURE  populate_question;
-		PROCEDURE  populate_rounds
-		
 
     PROCEDURE READ_FROM_FILE(DICTIONARY OUT KVDICT, FILE_NAME VARCHAR2);
 		PROCEDURE LOAD_DATA_FROM_CSV(FILE_NAME VARCHAR2, TAB_NAME VARCHAR2);
 		FUNCTION  ISDATE(P_DATE VARCHAR2) RETURN INTEGER;
+
+		PROCEDURE  populate_players;
+		PROCEDURE  populate_questions;
+		PROCEDURE  populate_rounds;
 
 END data_manipulation;
 /
@@ -267,65 +266,65 @@ IS
   EXCEPTION
       WHEN TWExceptions.TABLE_INEXISTENT THEN
           RAISE_APPLICATION_ERROR(-20005, 'The table ' || TAB_NAME || ' does not exist.');
-	END;
+	END LOAD_DATA_FROM_CSV;
 
 
-CREATE OR REPLACE PROCEDURE  populate_player AS
-  v_level Players.playerlevel%TYPE;
-  v_max_player Players.playerID%TYPE:=playerID_seq.currval;
-  BEGIN
+	PROCEDURE  populate_players AS
+	  v_level Players.playerlevel%TYPE;
+	  v_max_player Players.playerID%TYPE:=playerID_seq.currval;
+	  BEGIN
 
-    FOR i IN 1..5000 LOOP
-
-      
-            EXECUTE IMMEDIATE 'INSERT INTO GameUsers (Username,Email,Password) VALUES(' 
-                           || ''''||dbms_random.string('U', 20)||''''|| ','
-                           || ''''||dbms_random.string('U', 20)||'@gmail.com'||''''|| ','
-                           || ''''||dbms_random.string('U', 20)||''''||
-                           ')';
-  
-           v_level:=DBMS_RANDOM.value(1,10);
-           UPDATE players 
-           SET experience=50* POWER(2,v_level-1), playerlevel=v_level 
-           WHERE playerid=v_max_player+i;
-      
-  END LOOP;
-  
-END  populate_player;
-
-CREATE OR REPLACE PROCEDURE  populate_question AS
-
-  BEGIN
-  
-  FOR i IN 1 ..1000 LOOP
-        EXECUTE IMMEDIATE 'INSERT INTO questions (question,ANSWERA, ANSWERB,ANSWERC,ANSWERD,CORRECTANSWER,ROUNDID ) VALUES(' 
-                           || ''''||dbms_random.string('U', 20)||''''|| ','
-                           || ''''||dbms_random.string('L', 5)||''''|| ','
-                           || ''''||dbms_random.string('L', 5)||''''|| ','
-                           || ''''||dbms_random.string('L', 5)||''''|| ','
-                           || ''''||dbms_random.string('L', 5)||''''|| ','
-                           || ''''||dbms_random.VALUE(1, 4)||''''|| ','
-                           || ''''||dbms_random.VALUE(1, 100)||''''||
-                           ')';
-  END LOOP;
-END  populate_question;
-
-CREATE OR REPLACE PROCEDURE  populate_rounds AS
-
-  BEGIN
-  
-    FOR i IN 1..200 LOOP
-
-     EXECUTE IMMEDIATE 'INSERT INTO rounds (name,course) VALUES(' 
-                           || ''''||dbms_random.string('L', 20)||''''|| ','
-                           || ''''||dbms_random.string('U', 20)||''''||
-                           ')';
-  END LOOP;
-  
- 
-END  populate_rounds;
+	    FOR i IN 1..5000 LOOP
 
 
+	            EXECUTE IMMEDIATE 'INSERT INTO GameUsers (Username,Email,Password) VALUES('
+	                           || ''''||dbms_random.string('U', 20)||''''|| ','
+	                           || ''''||dbms_random.string('U', 20)||'@gmail.com'||''''|| ','
+	                           || ''''||dbms_random.string('U', 20)||''''||
+	                           ')';
+
+	           v_level:=DBMS_RANDOM.value(1,10);
+	           UPDATE players
+	           SET experience=50* POWER(2,v_level-1), playerlevel=v_level
+	           WHERE playerid=v_max_player+i;
+
+	  END LOOP;
+
+	END  populate_players;
+
+
+	PROCEDURE  populate_questions AS
+
+	  BEGIN
+
+	  FOR i IN 1 ..1000 LOOP
+	        EXECUTE IMMEDIATE 'INSERT INTO questions (question,ANSWERA, ANSWERB,ANSWERC,ANSWERD,CORRECTANSWER,ROUNDID ) VALUES('
+	                           || ''''||dbms_random.string('U', 20)||''''|| ','
+	                           || ''''||dbms_random.string('L', 5)||''''|| ','
+	                           || ''''||dbms_random.string('L', 5)||''''|| ','
+	                           || ''''||dbms_random.string('L', 5)||''''|| ','
+	                           || ''''||dbms_random.string('L', 5)||''''|| ','
+	                           || ''''||dbms_random.VALUE(1, 4)||''''|| ','
+	                           || ''''||dbms_random.VALUE(1, 100)||''''||
+	                           ')';
+	  END LOOP;
+	END  populate_questions;
+
+
+	PROCEDURE  populate_rounds AS
+
+	  BEGIN
+
+	    FOR i IN 1..200 LOOP
+
+	     EXECUTE IMMEDIATE 'INSERT INTO rounds (name,course) VALUES('
+	                           || ''''||dbms_random.string('L', 20)||''''|| ','
+	                           || ''''||dbms_random.string('U', 20)||''''||
+	                           ')';
+	  END LOOP;
+
+
+	END  populate_rounds;
 
 END data_manipulation;
 /
@@ -340,6 +339,7 @@ BEGIN
 --     data_manipulation.READ_FROM_FILE(DICTIONARY, 'PLAYERS.CSV');
     data_manipulation.LOAD_DATA_FROM_CSV('GAMEUSERS.csv', 'GAMEUSERS');
 END;
+/
 
 
 
@@ -349,5 +349,7 @@ DECLARE
 BEGIN
     RES := TO_DATE(V_DATE, 'YYYY-MM-DD HH:MI:SS');
 END;
+/
 
 SELECT COLUMN_NAME, DATA_TYPE FROM USER_tab_COLUMNS WHERE TABLE_NAME = 'PLAYERSSTATISTICS';
+/
--- DB_Oracle/Project_triggers.sql
+++ DB_Oracle/Project_triggers.sql
@@ -17,5 +17,161 @@ BEGIN
 END;
 /
 
+-- THIS TRIGGER LOGS INFORMATION WHEN AN USER REGISTERS / UPDATES PERSONAL INFO / DELETES HIS ACCOUNT
+CREATE OR REPLACE TRIGGER T_LOGGER_GAMEUSERS
+      AFTER
+        INSERT OR
+        UPDATE OR
+        DELETE
+      ON GAMEUSERS
+      FOR EACH ROW
+DECLARE
+      ALREADY_UPDATED   NUMBER(1);
+      OPERATION         VARCHAR2(1000);
+      DATE_TO_INSERT    DATE;
+BEGIN
+      CASE
+        -- ####################################
+        -- ######## ON INSERT #################
+        -- ####################################
+        WHEN INSERTING THEN
+
+            -- add something default to data_notare if inexistent
+           OPERATION := 'User with USERNAME ' || :NEW.USERNAME || ', MAIL ' || :NEW.EMAIL || ' registered. ID assigned: ' || :new.PLAYERID;
+
+            EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :OPERATION)' USING SYSDATE, OPERATION;
+
+        -- ####################################
+        -- ######## ON UPDATE #################
+        -- ####################################
+        WHEN UPDATING THEN
+
+            OPERATION := 'User with USERNAME ' || :old.USERNAME || ', MAIL ' || :old.EMAIL || ' updated info to: '  || :new.USERNAME || ', ' || :new.EMAIL || ', ' || :new.FACEBOOKID;
+
+            EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :OPERATION)' USING SYSDATE, OPERATION;
+
+        -- ####################################
+        -- ######## ON DELETE #################
+        -- ####################################
+        WHEN DELETING THEN
+
+
+          OPERATION := 'Player ' || :old.USERNAME || ' deleted his account.';
+          EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :QUERY_STR)' USING SYSDATE, OPERATION;
+
+      END CASE;
+EXCEPTION
+      -- EXCEPTION WHEN LOGGER TABLE DOES NOT EXIST. STILL, DO NOT INTRERUPT THE CURRENT OP.
+      WHEN OTHERS THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot log info about operation because logger table does not exist.');
+END;
+
+
+-- THIS TRIGGER LOGS INFORMATION WHEN AN USER UPGRADES (PLAYERS TABLE)
+CREATE OR REPLACE TRIGGER T_LOGGER_PLAYERS
+      AFTER
+        UPDATE
+      ON PLAYERS
+      FOR EACH ROW
+DECLARE
+      OPERATION         VARCHAR2(1000);
+BEGIN
+        -- ####################################
+        -- ######## ON UPDATE #################
+        -- ####################################
+
+        IF TRIM(:OLD.PLAYERNAME) <> TRIM(:NEW.PLAYERNAME)
+        THEN
+            OPERATION := 'Player with name ' || :old.PLAYERNAME || ' just updated it''s player name to ' || :new.PLAYERNAME;
+        ELSE
+            OPERATION := 'User with USERNAME ' || :old.PLAYERNAME || ' just upgraded.
+            New data: ' || :new.EXPERIENCE || ', ' || :NEW.PLAYERLEVEL || ', ' || :NEW.COOKIES || ', ' || :NEW.S_CHEAT || ', ' || :NEW.S_LUCK || ', ' || :NEW.S_TIME || ', ' || :NEW.SKILLPOINTS ;
+        END IF;
+
+        EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :OPERATION)' USING SYSDATE, OPERATION;
+
+EXCEPTION
+      -- EXCEPTION WHEN LOGGER TABLE DOES NOT EXIST. STILL, DO NOT INTRERUPT THE CURRENT OP.
+      WHEN OTHERS THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot log info about operation because logger table does not exist.');
+END;
+
+
+-- THIS TRIGGER LOGS INFORMATION WHEN AN USER wins / loses / logins (PLAYERS TABLE)
+CREATE OR REPLACE TRIGGER T_LOGGER_STATISTICS
+      AFTER
+        UPDATE
+      ON PLAYERSSTATISTICS
+      FOR EACH ROW
+DECLARE
+      OPERATION         VARCHAR2(5000);
+      P_PNAME           PLAYERS.PLAYERNAME%TYPE;
+BEGIN
+        SELECT PLAYERNAME INTO P_PNAME FROM PLAYERS WHERE PLAYERID = :OLD.PLAYERID;
+
+        -- ####################################
+        -- ######## ON UPDATE #################
+        -- ####################################
+        OPERATION := '';
+        IF :OLD.WINS <> :NEW.WINS THEN
+            OPERATION := 'Player named ' || P_PNAME || ' just WON a game.';
+        END IF;
+
+        IF :OLD.LOSES <> :NEW.LOSES THEN
+            OPERATION := 'Player named ' || P_PNAME || ' just LOST a game.';
+        END IF;
+
+        IF :OLD.LASTLOGINDATE <> :NEW.LASTLOGINDATE THEN
+            OPERATION := 'Player named ' || P_PNAME || ' just LOGGED IN.';
+        END IF;
+
+        IF :OLD.PERFECTROUNDS <> :NEW.PERFECTROUNDS THEN
+            OPERATION := 'Player named ' || P_PNAME || ' upgraded perfect rounds.';
+        END IF;
+
+        IF OPERATION IS NOT NULL
+        THEN
+            EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :OPERATION)' USING SYSDATE, OPERATION;
+        END IF;
+
+EXCEPTION
+      -- EXCEPTION WHEN LOGGER TABLE DOES NOT EXIST. STILL, DO NOT INTRERUPT THE CURRENT OP.
+      WHEN NO_DATA_FOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot retrieve data about the player from the players table;');
+      WHEN OTHERS THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot log info about operation because logger table does not exist.');
+END;
+
+
+-- THIS TRIGGER LOGS INFORMATION WHEN two users became friends
+CREATE OR REPLACE TRIGGER T_LOGGER_FRIENDS
+      AFTER
+        INSERT
+      ON FRIENDS
+      FOR EACH ROW
+DECLARE
+      OPERATION          VARCHAR2(1000);
+      P_FPNAME           PLAYERS.PLAYERNAME%TYPE;
+      P_SPNAME           PLAYERS.PLAYERNAME%TYPE;
+BEGIN
+        SELECT PLAYERNAME INTO P_FPNAME FROM PLAYERS WHERE PLAYERID = :NEW.PLAYER1ID;
+        SELECT PLAYERNAME INTO P_SPNAME FROM PLAYERS WHERE PLAYERID = :NEW.PLAYER2ID;
+
+        -- ####################################
+        -- ######## ON UPDATE #################
+        -- ####################################
+
+        OPERATION := 'Player ' || P_FPNAME || ' just became friend with ' || P_SPNAME;
+
+        EXECUTE IMMEDIATE 'INSERT INTO LOGGER_TABLE VALUES(:DATE_ADDED, :OPERATION)' USING SYSDATE, OPERATION;
+
+EXCEPTION
+      -- EXCEPTION WHEN LOGGER TABLE DOES NOT EXIST. STILL, DO NOT INTRERUPT THE CURRENT OP.
+      WHEN NO_DATA_FOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot retrieve data about the player from the players table;');
+      WHEN OTHERS THEN
+          DBMS_OUTPUT.PUT_LINE('Cannot log info about operation because logger table does not exist.');
+END;
+
 
 commit;
