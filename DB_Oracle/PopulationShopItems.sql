8bc655786b04e125ac2917ee466b1d648b3b1d8d
Chinmaiy <cezara.cbz@gmail.com>
2016-06-17 16:48:07 +0300

    Added sql script for items.

---
+++ DB_Oracle/PopulationShopItems.sql
@@ -0,0 +1,30 @@
+INSERT INTO ITEMS (NAME, DESCRIPTION, SKILLPOINTS, SKILL, COOKIESCOST, FILEPATH)
+  VALUES('Brown Hour Glass', 
+         'This brown hour glass will give you time skill points.',
+         3,
+         'S_TIME',
+         20,
+         'images/items/brown_hour_glass.svg');
+/
+
+INSERT INTO ITEMS (NAME, DESCRIPTION, SKILLPOINTS, SKILL, COOKIESCOST, FILEPATH)
+  VALUES('Blue Hour Glass', 
+         'This blue hour glass will give you time skill points.',
+         5,
+         'S_TIME',
+         40,
+         'images/items/blue_hour_glass.svg');
+/
+
+INSERT INTO ITEMS (NAME, DESCRIPTION, SKILLPOINTS, SKILL, COOKIESCOST, FILEPATH)
+  VALUES('Green Hour Glass', 
+         'This green hour glass will give you time skill points.',
+         7,
+         'S_TIME',
+         60,
+         'images/items/green_hour_glass.svg');
+/
+
+COMMIT;
+
+
