56d93e274b7e2e60accff275809de32e3c4ff931
Marian FX <marian.focsa@outlook.com>
2017-05-06 23:52:05 +0300

    Added Local to be included.

---
+++ config/local.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+
+    port: 6996,
+    //Try with require("os").hostname()
+    serverURL: "http://192.168.0.100:6996" ,
+    mysql: {
+        adapter: 'sails-mysql',
+        host: "127.0.0.1",
+        user: 'fx', //optional
+        password: 'fxfxfx', //optional
+        database: 'funweb' //optional
+    },
+    mysql_full: {
+        host: "127.0.0.1",
+        user: 'fx', //optional
+        password: 'fxfxfx', //optional
+        database: 'funweb', //optional
+        multipleStatements: true
+    },
+};
